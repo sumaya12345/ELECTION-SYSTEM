6fead4d087c6a842d9a2f21c91fd3fc3b2bd1719
sumaya12345 <asmiina99@gmail.com>
2025-09-18 09:40:19 +0300

    full project

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,15 +5,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 // Public routes
-import Home from "./Pages/home/Home";
-import Login from "./Pages/home/Login";
-import Register from "./Pages/home/Register";
+import Home from "./Pages/ome/Home";
+import Login from "./Pages/Home/Login";
+import Register from "./Pages/Home/Register";
 import About from "./Pages/Home/about";
 import Contact from "./Pages/home/Contact";
 import Candidates from "./Pages/canditate/canditates";
 import ActiveElectionsPage from "./Components/active_election";
 import CandidateActiveElections from "./Pages/canditate/CandidateActiveElections";
-import CandidateLogin from "./Pages/home/CandidateLogin"
+import CandidateLogin from "./Pages/Home/CandidateLogin"
 // Admin routes
 import AdminLogin from "./Pages/admin/AdminLogin";
 import AdminDashboard from "./Pages/admin/adminDashboard";
